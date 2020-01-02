5dd228f21ee6912146cc128e56ec24587c6a6ed1
JJRWalker <josh.walker@outlook.com>
2020-01-02 19:38:49 +0000

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,8 +23,3 @@ project "ImGui"
     
 	filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
